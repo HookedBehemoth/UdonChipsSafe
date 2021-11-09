6d6c8832465e9b687a5a213973394015d89f08d5
HookedBehemoth <LuisScheurenbrand@gmail.com>
2021-11-09 02:42:24 +0100

    remove unneccessary namespace

--- UdonChipsSafe.cs
+++ UdonChipsSafe.cs
@@ -7,7 +7,7 @@ using static FakeUdon.FakeUdonRegistry;
 namespace UdonChipsSafe {
     public class Starter : MelonMod {
         public override void OnApplicationStart() {
-            RegisterType<UdonChipsSafe.UdonChips>("c266c8322c0c75349b2f2d9a1c44aec1");
+            RegisterType<UdonChips>("c266c8322c0c75349b2f2d9a1c44aec1");
         }
     }
 }
