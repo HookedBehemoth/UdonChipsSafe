6bb09c5fd44cf96f413953fe56052967b6654abc
HookedBehemoth <LuisScheurenbrand@gmail.com>
2021-11-09 02:07:44 +0100

    add UdonChipsSafe

---
+++ UdonChips.cs
@@ -0,0 +1,27 @@
+using UdonSharp;
+using MelonLoader;
+
+namespace UdonChipsSafe {
+	public class UdonChips : UdonSharpBehaviour {
+        private const string Category = nameof(UdonChips);
+        private const string Name = "Money";
+        static MelonPreferences_Entry<float> entry;
+		public float money = 1000;
+
+		public string format = "$ {0:F0}";
+
+        static bool loaded = false;
+        public void Start() {
+            if (!loaded) {
+                MelonPreferences.CreateCategory(Category);
+                entry = MelonPreferences.CreateEntry(Category, Name, money);
+                loaded = true;
+            }
+            money = entry.Value;
+        }
+        public void OnDestroy() {
+            entry.Value = money;
+            MelonPreferences.Save();
+        }
+	}
+}
---
+++ UdonChipsSafe.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+using static FakeUdon.FakeUdonRegistry;
+
+[assembly: MelonInfo(typeof(UdonChipsSafe.Starter), nameof(UdonChipsSafe), "1.0", "Behemoth")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace UdonChipsSafe {
+    public class Starter : MelonMod {
+        public override void OnApplicationStart() {
+            RegisterType<UdonChipsSafe.UdonChips>("c266c8322c0c75349b2f2d9a1c44aec1");
+        }
+    }
+}
